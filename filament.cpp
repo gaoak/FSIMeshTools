7201cde393e8b2aa3a2b90745bf8b3a5faa32be9
Ankang Gao <ankang.gao@imperial.ac.uk>
2022-05-08 21:57:40 +0800

    filament mesh

--- filament.cpp
+++ filament.cpp
@@ -2,7 +2,7 @@
 #include<fstream>
 #include<string>
 #include<vector>
-#include<math>
+#include<cmath>
 #define SEGTYPE 2
 using namespace std;
 
@@ -31,15 +31,16 @@ void GenerateElements(vector<vector<double> > &points, vector<vector<int> > &ele
     boundCondition.clear();
     elements.push_back(vector<int>());
     boundCondition.push_back(vector<int>());
-    size_t Ne = points.size() - 2;
-    for(size_t i=1; i<=Ne; ++i) {
+    int Ne = (int)points.size() - 2;
+    for(int i=1; i<=Ne; ++i) {
         vector<int> p = {i, i, i+1, i+1, SEGTYPE, 1};
         elements.push_back(p);
     }
-    for(size_t i=1; i<points.size(); ++i) {
+    for(int i=1; i<(int)points.size(); ++i) {
         vector<int> p = {i, 0, 0, 0, 0, 0, 0};
         boundCondition.push_back(p);
     }
+    boundCondition[1][1] = 1;
 }
 
 void Output(string filename, vector<vector<double> > &points, vector<vector<int> > &ele, vector<vector<int> > &bc) {
@@ -47,37 +48,49 @@ void Output(string filename, vector<vector<double> > &points, vector<vector<int>
     string endtag("END");
     ofstream ofile(filename.c_str());
     ofile << "Frame3D\n";
-    sprintf(buff, "%5d %5d %5d\n", int points.size() - 1, int ele.size() - 1, 1);
+    sprintf(buff, "%5d %5d %5d\n", int(points.size()) - 1, int(ele.size()) - 1, 1);
     ofile << buff << endtag << "\n"; 
     //output points
-    sprintf(buff, "%5d %5s %5s %5s\n", int points.size()-1, "X", "Y", "Z");
+    sprintf(buff, "%5d %22s %22s %22s\n", int(points.size())-1, "X", "Y", "Z");
     ofile << buff;
     for(size_t i=1; i<points.size(); ++i) {
-        sprintf(buff, "%5d %22.18f %22.18f %22.18f\n", i, points[i][0], points[i][1], points[i][2]);
+        sprintf(buff, "%5d %22.18f %22.18f %22.18f\n", (int)i, points[i][0], points[i][1], points[i][2]);
         ofile << buff;
     }
     ofile << endtag << "\n";
     // output elements
-    sprintf(buff, "%5d %5s %5s %5s %5s %5s\n", int ele.size() - 1, "I", "J", "K", "TYPE", "MAT");
+    sprintf(buff, "%5d %5s %5s %5s %5s %5s\n", int(ele.size()) - 1, "I", "J", "K", "TYPE", "MAT");
     ofile << buff;
     for(size_t i=1; i<ele.size(); ++i) {
-        sprintf(buff, "%5d %5d %5d %5d %5d %5d\n", ele[i][0], ele[i][1], ele[i][2], ele[i][3], ele[i][4]. ele[i][5]);
+        sprintf(buff, "%5d %5d %5d %5d %5d %5d\n", ele[i][0], ele[i][1], ele[i][2], ele[i][3], ele[i][4], ele[i][5]);
         ofile << buff;
     }
     ofile << endtag << "\n";
     // output boundary conditions
-    sprintf(buff, "%5d %5s %5s %5s %5s %5s %5s\n", int points.size() - 1, "XTRA", "YTRA", "ZTRA", "XROT", "YROT", "ZROT");
+    sprintf(buff, "%5d %5s %5s %5s %5s %5s %5s\n", int(points.size()) - 1, "XTRA", "YTRA", "ZTRA", "XROT", "YROT", "ZROT");
     ofile << buff;
     for(size_t i=1; i<points.size(); ++i) {
-        sprintf(buff, "%5d %5d %5d %5d %5d %5d %5d\n", int i, bc[i][0], bc[i][1], bc[i][2], bc[i][3], bc[i][4], bc[i][5]);
+        sprintf(buff, "%5d %5d %5d %5d %5d %5d %5d\n", bc[i][0], bc[i][1], bc[i][2], bc[i][3], bc[i][4], bc[i][5], bc[i][6]);
         ofile << buff;
     }
     ofile << endtag << "\n";
     // output property parameters
+    ofile << "1   E           G           A           RHO         GAMMA       IP          IA          IB" << "\n";
+    ofile << "1   0.100D+01   0.100D+01   0.100D+01   0.100D+01   0.100D+01   0.100D+01   0.150D+01   0.500D+00" << "\n";
+    ofile << endtag << endl;
     ofile.close();
 }
 
 int main() {
-    GeneratePoints
+    double radius = 0.5;
+    int Np = 101;
+    string filename("Beam.dat");
+    vector<vector<double> > points;
+    vector<vector<int> > elements;
+    vector<vector<int> > boundCondition;
+    GeneratePoints(Np, radius, points);
+    GenerateElements(points, elements, boundCondition);
+    Output(filename, points, elements, boundCondition);
+    
     return 0;
 }
