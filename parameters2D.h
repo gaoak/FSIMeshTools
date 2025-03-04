ebf68216080b46af8be4a9dce2efbe0e737853e5
Small Luo <xianguangLuo@mail.ustc.edu.cn>
2025-03-04 13:33:26 +0800

    add spanwise direction and more shapes

--- parameters2D.h
+++ parameters2D.h
@@ -10,7 +10,6 @@
 #define YMAX 20.
 /// end of description
 
-
 /// generates nonuniform fluid mesh
 /// nonuniformfluid2D.cpp
 /// user defined parameters
@@ -22,18 +21,15 @@ std::vector<int> NyFMesh{  10, 410, 680};
 /// Generates the body mesh (open or closed)
 /// filament.cpp
 /// user defined parameters
-/*
-#define NPOINTS 201
-#define GEOMTYPE   2 // 0 cylinder; 1 vertical line; 2 horizontal line; 3 horizontal cos
-#define PARAMS0    1 //maximum length
-#define PARAMS1    0.1 //cos amplitude
-#define CLOSED false */
-
-#define NPOINTS 101
-#define GEOMTYPE   2// 0 cylinder; 1 vertical line; 2 horizontal line;
-                     //3 horizontal cos; 4 horizontal square wave
-std::vector<double> filaparams{1., 0., 1}; //length, amplitude, length1, length2
-std::vector<int> spantype{0, 0}; // right(0 const; 1 oblique line; 2 cos), left(...)
-std::vector<std::vector<double>> spanparams{{0.0, 1.0, 0.0, 0.0}, {1.0, 1.0, 0.0, 0.0}}; // left(amplitude, frequency, phase, intercept), right(...)
 #define CLOSED false
-/// end of description
+#define NPOINTS 101
+#define GEOMTYPE 2    // 0 cylinder; 1 vertical line; 2 horizontal line; 3 horizontal cos; 4 horizontal square wave
+std::vector<double> filaparams{1, 0., 1}; //length, amplitude, length1, length2
+std::vector<double> normal{0, 0, 1}; // extension direction
+std::vector<int> spantype{4, 4}; // right(0 const; 1 oblique line; 2 ellipse; 3 cos; 4 sawtooth), left(...)
+std::vector<std::vector<double>> spanparams{{0.25, 0.20, 0.10, 0.0}, {0.25, 0.20, 0.10, 0.0}}; 
+//0 left(leading span length, none, none, none), right(...)
+//1 left(leading span length, slope, none, none), right(...)
+//2 left(leading span length, chord center, chord radius, spanwise radius), right(...)
+//3 left(leading span length, amplitude, frequency, phase), right(...)
+//4 left(leading span length, saw width, saw height, none), right(...)
